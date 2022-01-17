374362274e560f6584c1459af21e92491dca716a
Ariv Debnath <arivdebnath10@gmail.com>
2022-01-17 22:26:46 +0530

    added Get avatar endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -184,4 +184,19 @@ userRouter.delete('/users/me/avatars', auth, async (req, res) => {
     }
 })
 
+userRouter.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!(user && user.avatar)) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/jpg');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send();
+    }
+})
+
 module.exports = userRouter;
