cfd90cced6a2402a815cd25fdfa7114e485bac06
Ariv Debnath <arivdebnath10@gmail.com>
2022-01-22 22:36:55 +0530

    added tests for login endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,14 +2,16 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
-// beforeEach(async () => {
-//     await request(app).pos
-// })
+const testUserOne = {
+    name: "Test User One",
+    email: "testuser1@email.com",
+    password: "testuserone",
+}
 
-afterEach(async () => {
-    await User.findOneAndDelete({
-        email: "supertest@email.com",
-    })
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(testUserOne).save();
+
 });
 
 test('signup test', async () => {
@@ -19,3 +21,17 @@ test('signup test', async () => {
         password: "supertest"
     }).expect(201);
 })
+
+test('signin test', async () => {
+    await request(app).post('/users/login').send({
+        email: testUserOne.email,
+        password: testUserOne.password,
+    }).expect(200);
+});
+
+test('login fail test', async ()=>{
+    await request(app).post('/users/login').send({
+        email: testUserOne.email,
+        password: "wrongCredential",
+    }).expect(400);
+});
