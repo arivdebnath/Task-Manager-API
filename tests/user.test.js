500acce2010f2d87647a8be60541a437a2a28201
Ariv Debnath <arivdebnath10@gmail.com>
2022-01-23 23:26:41 +0530

    added tests for read profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const testUserOneId = new mongoose.Types.ObjectId();
 const testUserOne = {
+    _id: testUserOneId,
     name: "Test User One",
     email: "testuser1@email.com",
     password: "testuserone",
+    tokens: [{
+        _id: new mongoose.Types.ObjectId(),
+        token: jwt.sign({ _id: testUserOneId }, process.env.HASH_SECR),
+    }]
 }
 
 beforeEach(async () => {
@@ -29,9 +37,25 @@ test('signin test', async () => {
     }).expect(200);
 });
 
-test('login fail test', async ()=>{
+test('login fail test', async () => {
     await request(app).post('/users/login').send({
         email: testUserOne.email,
         password: "wrongCredential",
     }).expect(400);
 });
+
+test('read profile test', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
+        .send()
+        .expect(200);
+})
+
+test('read profile fail test', async ()=>{
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401);
+})
+
