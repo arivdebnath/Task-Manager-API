d37923ff34d49b8290ac4ec55455d1b484bcd44d
Ariv Debnath <arivdebnath10@gmail.com>
2022-01-24 00:31:19 +0530

    updated tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
+const userRouter = require('../src/routes/user');
 
 const testUserOneId = new mongoose.Types.ObjectId();
 const testUserOne = {
@@ -23,18 +24,38 @@ beforeEach(async () => {
 });
 
 test('signup test', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: "supertest user",
         email: "supertest@email.com",
         password: "supertest"
     }).expect(201);
+
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'supertest user',
+            email: 'supertest@email.com',
+        },
+        token: user.tokens[0].token,
+    })
+
+    expect(user.password).not.toBe('supertest');
+
 })
 
 test('signin test', async () => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: testUserOne.email,
         password: testUserOne.password,
     }).expect(200);
+
+    const user = await User.findById(response.body.user._id);
+    expect(user).not.toBeNull();
+
+    // expect(user.tokens[1].token).toBe(response.body.token);
+    expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('login fail test', async () => {
@@ -60,11 +81,14 @@ test('read profile fail test', async () => {
 });
 
 test('delete profile test', async () => {
-    await request(app)
+    const response = await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${testUserOne.tokens[0].token}`)
         .send()
         .expect(200);
+    
+    const user = await User.findById(testUserOneId);
+    expect(user).toBeNull();
 });
 
 test('delete profile fail test', async () => {
